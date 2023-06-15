929fd85efb8e9e30338ff0f0d883ff894dfb8e63
king <akingbenidavidoyin@gmail.com>
2023-06-15 19:30:38 +0000

    correct 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+/** 
+*main - prints a string with put
+*
+* Return: integer 0
+*/
 int main(void)
 {
-/* prints a string with put*/;
 puts("\"Programming is like building a multilingual puzzle");
-return 0;
+return (0);
 }
