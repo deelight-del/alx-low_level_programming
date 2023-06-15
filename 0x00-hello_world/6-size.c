8053c07de6adfc7381c13d2d01519030d7c20f2d
king <akingbenidavidoyin@gmail.com>
2023-06-15 20:04:37 +0000

    6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/** main - returns 0 but prinss
+*
+*Return: returns int 0
+*/
+
+int main(void)
+{
+char i;
+int j;
+long long int k;
+float l;
+printf("Size of a char: %d byte(s)\n
+	Size of an int: %d byte(s)\n
+	Size of a long long int: %d bytess)\n
+	Size of a float: %d byte(s)\n", sizeof(i), sizeof(j), sizeof(k), sizeof(l));
+return (0);
+}
