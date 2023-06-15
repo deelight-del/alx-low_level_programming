f00bf9f1fb598d4953649866521420176a6299f3
king <akingbenidavidoyin@gmail.com>
2023-06-15 19:38:44 +0000

    5

---
+++ 0x00-hello_world/5-print.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main - just returns an integer
+*Return: Returns o
+*/
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
