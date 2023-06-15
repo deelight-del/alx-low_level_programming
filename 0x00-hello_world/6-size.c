4e6b6bdb9ede41d7cec3f15e7df7734329dd4abf
king <akingbenidavidoyin@gmail.com>
2023-06-15 23:22:10 +0000

    space removed

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ int j;
 long int k;
 long long int l;
 float m;
-printf("Size of a char: %lu byte(s)\nSize of an int: %lu byte(s)\n
+
+printf("Size of a char: %lu byte(s)\nSize of an int: %lu byte(s)\n\
 Size of a long int: %lu byte(s)\nSize of a long long int: %lu byte(s)\nSize \
 a float: %lu byte(s)\n",
-sizeof(i), sizeof(j), sizeof(k), sizeof(l), sizeof(m)
-);
+sizeof(i), sizeof(j), sizeof(k), sizeof(l), sizeof(m));
 return (0);
 }
