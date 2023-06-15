479cb5a753608d861ce34fc9574f38ea0d465aaf
king <akingbenidavidoyin@gmail.com>
2023-06-15 23:11:33 +0000

    remove trail space

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
-/** main - Prints sizes
+/**
+*main - Prints sizes
 *
-*Return: returns int 0
+*Return: 0(Success)
 */
 
 int main(void)
@@ -11,8 +12,10 @@ int j;
 long int k;
 long long int l;
 float m;
-printf("Size of a char: %lu byte(s)\nSize of an int: %lu byte(s)\n\
-Size of a long int: %lu byte(s)\nSize of a long long int: %lu byte(s)\nSize of\
- a float: %lu byte(s)\n", sizeof(i),sizeof(j), sizeof(k), sizeof(l), sizeof(m));
+printf("Size of a char: %lu byte(s)\nSize of an int: %lu byte(s)\n
+Size of a long int: %lu byte(s)\nSize of a long long int: %lu byte(s)\nSize \
+a float: %lu byte(s)\n",
+sizeof(i), sizeof(j), sizeof(k), sizeof(l), sizeof(m)
+);
 return (0);
 }
