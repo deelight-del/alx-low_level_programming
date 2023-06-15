c774dbf3a33d6b42010cc83ddeb7e18bb85c6749
king <akingbenidavidoyin@gmail.com>
2023-06-15 20:22:17 +0000

    mm

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ long int k;
 long long int l;
 float m;
 printf("Size of a char: %lu byte(s)\n\
-	Size of an int: %lu byte(s)\n\
-	Size of long int: %lu byte(s)\n\
-	Size of a long long int: %lu byte(s)\n\
-	Size of a float: %lu byte(s)\n", sizeof(i),\
-	sizeof(j), sizeof(k), sizeof(l), sizeof(m));
+Size of an int: %lu byte(s)\n\
+Size of long int: %lu byte(s)\n\
+Size of a long long int: %lu byte(s)\n\
+Size of a float: %lu byte(s)", sizeof(i),\
+sizeof(j), sizeof(k), sizeof(l), sizeof(m));
 return (0);
 }
