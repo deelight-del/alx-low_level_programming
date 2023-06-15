5794519a7de664c1973f5dabc2d06e15f1f9a6cf
king <akingbenidavidoyin@gmail.com>
2023-06-15 20:15:22 +0000

    unk

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,9 +10,9 @@ char i;
 int j;
 long long int k;
 float l;
-printf("Size of a char: %d byte(s)\n
-	Size of an int: %d byte(s)\n
-	Size of a long long int: %d bytess)\n
-	Size of a float: %d byte(s)\n", sizeof(i), sizeof(j), sizeof(k), sizeof(l));
+printf("Size of a char: %lu byte(s)\n\
+		Size of an int: %lu byte(s)\n\
+		Size of a long long int: %lu bytess)\n\
+	Size of a float: %lu byte(s)\n", sizeof(i), sizeof(j), sizeof(k), sizeof(l));
 return (0);
 }
