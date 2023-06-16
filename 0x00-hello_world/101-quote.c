ecfc1116907501607a8a04bf7bd61f21ebc78d62
king <akingbenidavidoyin@gmail.com>
2023-06-16 06:11:04 +0000

    101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+*main - A function that returns 1, and prints to std err
+*
+*Return: 1(Sucess)
+*/
+
+int main(void)
+
+{fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, \
+2015-10-19\n");
+return (1);
+}
