cc3c9cf42a07b42ac2538d3a323bb5d4c1f2d7ca
king <akingbenidavidoyin@gmail.com>
2023-06-16 21:02:20 +0000

    9 corrected

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,18 +8,13 @@
 int main(void)
 {
 int i;
-int j;
 for (i = 0; i < 10; i++)
 {
-for (j = 0; j < 10; j++)
-{
 putchar(i + '0');
-putchar(j + '0');
-if (i == 9 && j == 9)
+if (i == 9)
 	putchar('\n');
 else
 	putchar(',');
-}
 }
 return (0);
 }
