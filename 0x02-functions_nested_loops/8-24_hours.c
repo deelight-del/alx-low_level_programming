53cfa461f24f9261885b170dfa9cf7c7e13b42ba
king <akingbenidavidoyin@gmail.com>
2023-06-20 23:30:42 +0000

    8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include "main.h"
+/*jack_bauer - Prints every minute
+*
+*Return: returns void
+*/
+
+void jack_bauer(void)
+{
+int i;
+int j;
+int k;
+int m;
+int l;
+for (j = 0, j < 3; j++)
+
+{
+	if (j != 2)
+	{
+	for (k = 0; k < 10; k++)
+	{
+		for (l = 0; l < 6; l++)
+			for (m = 0; m < 10; m++)
+				_putchar(j + '0');
+				_putchar(k + '0');
+				_putchar(59);
+				_putchar(l + '0');
+				_putchar(m + '0');
+				_putchar('\n');
+
+
+	}
+	}
+	else
+	{
+	for (k = 0; k < 4; k++)
+	{
+	for (l = 0; l < 6; l++)
+		for (m = 0; m < 10; m++)
+			_putchar(j + '0');
+			_putchar(k + '0');
+			_putchar(59);
+			_putchar(l + '0');
+			_putchar(m + '0');
+			_putchar('\n');
+	}
+	}
+}
+return;
+}
