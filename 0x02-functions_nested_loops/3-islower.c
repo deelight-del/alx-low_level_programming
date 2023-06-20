8be3f60999065fe776f9e21c7ad87fcf5af21564
king <akingbenidavidoyin@gmail.com>
2023-06-20 19:53:58 +0000

    correct betty style

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include "main.h"
 /**
-_islower - checks if a given character is lower case or not
+*_islower - Returns 1 if character is lower case, and 0 otherwise
 *
 *Return: 1(lowercase), 0(if otherwise)
 */
 
 int _islower(int c)
+
 {if (c >= 97 && c < 123)
 	return (1);
 else
