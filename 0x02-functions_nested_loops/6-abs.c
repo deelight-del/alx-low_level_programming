84fa01a1e546192bd839f706db260fe1fe2a9304
king <akingbenidavidoyin@gmail.com>
2023-06-20 20:44:25 +0000

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*_abs - returns the absolute of a given number
+*@n: The number to be checked
+*
+*Return: +n(if > 0), +0(if 0), and +n(if < 0)
+*/
+
+int print_sign(int n)
+{
+int ret;
+if (n >= 0)
+{
+ret = n;
+}
+else
+{
+ret = n * -1;
+}
+return ret;
+}
