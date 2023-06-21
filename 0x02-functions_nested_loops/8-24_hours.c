619eb9bc3666efcdd840fbbddda9fc25be06f5af
king <akingbenidavidoyin@gmail.com>
2023-06-21 00:15:33 +0000

    adjusted braces

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,44 +7,45 @@
 
 void jack_bauer(void)
 {
-int i;
+
 int j;
 int k;
 int m;
 int l;
+
 for (j = 0, j < 3; j++)
-
 {
-	if (j != 2)
-	{
-	for (k = 0; k < 10; k++)
-	{
-		for (l = 0; l < 6; l++)
-			for (m = 0; m < 10; m++)
-				_putchar(j + '0');
-				_putchar(k + '0');
-				_putchar(59);
-				_putchar(l + '0');
-				_putchar(m + '0');
-				_putchar('\n');
+if (j != 2)
+{
+for (k = 0; k < 10; k++)
+{
+for (l = 0; l < 6; l++)
+for (m = 0; m < 10; m++)
+_putchar(j + '0');
+_putchar(k + '0');
+_putchar(59);
+_putchar(l + '0');
+_putchar(m + '0');
+_putchar('\n');
+}
+}
 
-
-	}
-	}
-	else
-	{
-	for (k = 0; k < 4; k++)
-	{
-	for (l = 0; l < 6; l++)
-		for (m = 0; m < 10; m++)
-			_putchar(j + '0');
-			_putchar(k + '0');
-			_putchar(59);
-			_putchar(l + '0');
-			_putchar(m + '0');
-			_putchar('\n');
-	}
-	}
+else
+{
+for (k = 0; k < 4; k++)
+{
+for (l = 0; l < 6; l++)
+for (m = 0; m < 10; m++)
+{
+_putchar(j + '0');
+_putchar(k + '0');
+_putchar(59);
+_putchar(l + '0');
+_putchar(m + '0');
+_putchar('\n');
+}
+}
+}
 }
 return;
 }
