439edf8ed45f4cd1a1fdff8a1d91eda4ec05943d
king <akingbenidavidoyin@gmail.com>
2023-06-21 15:52:48 +0000

    13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main - computes and sums all multiples of 3 & 5 below 1024
+*
+*Return: 0(Success)
+*/
+
+int main(void)
+{
+int i, j;
+j = 0;
+for (i = 0; i <= 1024; i++);
+{
+if (i % 3 == 0 || i % 5 == 0)
+j = j + i;
+}
+printf("%d", j);
+printf("\n");
+return (0);
+}
