8c755eae4f02b038f9e170dc247fe660152c53d1
king <akingbenidavidoyin@gmail.com>
2023-06-22 11:42:50 +0000

    3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*print_numbers - Print numbers from 0 - 9
+*
+*Return: void
+*/
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar('0' + i);
+	}
+	_putchar('\n');
+return;
+}
