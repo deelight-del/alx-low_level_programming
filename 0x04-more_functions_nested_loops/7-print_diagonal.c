014ff356b091c42830e3ccdc13cad8e8dc0ad392
king <akingbenidavidoyin@gmail.com>
2023-06-22 13:37:52 +0000

    7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* print_diagonal - Print diagonal line of size n
+*@n: number of times to print diagonal line
+*Return: void
+*/
+
+void print_diagonal(int n)
+{
+	int i;
+	int k;
+
+	if (n > 0)
+	{
+		_putchar('\');
+		_putchar('\n');
+
+		k = 0;
+		for (i = 1; i <= n - 1; i++)
+		{
+			k++;
+			for (j = 1; j <= k; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\');
+			_puthchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+return;
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -6,3 +6,7 @@ void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
+void print_diagonal(int);
+void print_square(int);
+void print_triangle(int size);
+
