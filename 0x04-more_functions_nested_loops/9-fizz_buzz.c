ae50c508e95ba8a1e73a478f1d31451538e08174
king <akingbenidavidoyin@gmail.com>
2023-06-22 14:46:19 +0000

    9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+*main - prints character for fizz or buzz or number depending on cond
+*
+*Return: 0(Success)
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 5 == 0 && i % 3 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+
+		/* Next is control for what to come after respective chars*/
+
+		if (i == 100)
+		{
+			printf("\n");
+		}
+		else
+		{
+			printf(" ");
+		}
+	}
+}
