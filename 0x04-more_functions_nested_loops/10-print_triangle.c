f0e508370dcac14c5b6f461bfb272c8d0a6be1aa
king <akingbenidavidoyin@gmail.com>
2023-06-22 15:40:04 +0000

    10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*print_triangle - prints triangle with # and space
+*@size: size of triangle
+*
+*Return: void
+*/
+
+void print_triangle(int size)
+{
+	int i, j, deficit, space, k, space_off;
+
+	if (size == 1)
+	{
+		_putchar('#');
+		_putchar('\n');
+	}
+	else if (size > 1)
+	{
+		deficit = 0;
+		for (i = 1; i <= size; i++)
+		{
+			deficit++;
+			space = size - deficit;
+			for (j = 1; j <= space; j++)
+			{
+				_putchar(' ');
+			}
+			space_off = space + 1;
+			for (k = space_off; k <= size; k++)
+			{
+				_putchar('#');
+			}
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+return;
+}
