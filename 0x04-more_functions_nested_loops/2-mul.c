267ca289726b0007da583933d05619e9138858e9
king <akingbenidavidoyin@gmail.com>
2023-06-22 11:32:32 +0000

    2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**mul - returns the result of multipying two nummbers together
+*@a: first operand
+*@b: second operand
+*
+*Return: returns the result of multipying a and b
+*/
+
+int mul(int a, int b)
+{
+	int ret;
+	ret = a * b;
+	return (ret);
+}
