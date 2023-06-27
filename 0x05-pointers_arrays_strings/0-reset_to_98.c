24405e68d39e970c28b09842ac9b9f63877ad785
king <akingbenidavidoyin@gmail.com>
2023-06-27 01:55:45 +0000

    0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*reset_to_98 - resets variable to 98
+*@n: Operand pointer to reset
+*
+*Return: void
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+	return;
+}
