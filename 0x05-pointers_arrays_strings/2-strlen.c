fb32d10ca6974230ba5e5bcee2b0a81570e80133
king <akingbenidavidoyin@gmail.com>
2023-06-27 02:21:09 +0000

    2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,1 +1,21 @@
 #include <stdio.h>
+#include "main.h"
+/**
+*_strlen - returns length of the string
+*@s: pointer variable
+*
+*Return: int (length of string)
+*/
+
+int _strlen(char *s)
+{
+	int ret;
+	int i;
+
+	for (i = 0; s != NULL; i++)
+	{
+		s++; /* increase pointer by one */
+	}
+	ret = i;
+	return (ret);
+}
