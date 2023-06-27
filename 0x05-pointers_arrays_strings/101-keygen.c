65a9aca91cb45504fe781faa9e0358a00e0ecbc0
king <akingbenidavidoyin@gmail.com>
2023-06-27 20:11:52 +0000

    11

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define size 10
+
+/**
+*main - generates random pw that cracks
+*
+*Return: 0(success)
+*/
+
+int *main(void)
+{
+	int i;
+	static int Arr[size];
+
+	srand(time(NULL));
+
+	for (i = 0; i < size; i++)
+	{
+		Arr[i] = rand();
+	}
+return (Arr);
+}
