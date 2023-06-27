6ebde33591c711a2bb695757f5931ce7cc07380b
king <akingbenidavidoyin@gmail.com>
2023-06-27 04:32:19 +0000

    5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -3,3 +3,25 @@
 
 /**
 *rev_string - reverses a given string
+*@s: operand to reverse
+*
+*Return: void
+*/
+
+void rev_string(char *s)
+{
+	int len;
+	char rev[len];
+	char *p;
+	int i;
+
+	i = 0;
+
+	len = _strlen(s);
+
+	for (p = s + (len - 1); p >= s; p--)
+	{
+		rev[i] = *p;
+		i++;
+	}
+}
