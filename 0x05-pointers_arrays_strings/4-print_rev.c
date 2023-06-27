95237952d83aa9f77c655c3fa2f9b345b30f2f75
king <akingbenidavidoyin@gmail.com>
2023-06-27 03:56:47 +0000

    4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,3 +1,40 @@
 #include <stdio.h>
 #include "main.h"
 /**
+*_print_rev - prints string in reverse to stdout
+*@s: string to print
+*
+*Return: void
+*/
+
+void _print_rev(char *s)
+{
+	int i = _strlen(s);
+	int j;
+
+	for (j = i - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
+}
+
+/**
+*_strlen - returns length of the string
+*@s: pointer variable
+*
+*Return: int (length of string)
+*/
+
+int _strlen(char *s)
+{
+	int ret;
+	int i;
+
+	for (i = 0; *s != '\0'; i++)
+	{
+		s++; /* increase pointer by one */
+	}
+	ret = i;
+	return (ret);
+}
