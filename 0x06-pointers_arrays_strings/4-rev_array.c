d1bb768aca2fd09aa581bb7273f4c6a4d8c63199
king <akingbenidavidoyin@gmail.com>
2023-06-30 11:25:17 +0000

    change rev array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -17,14 +17,13 @@ void reverse_array(int *a, int n)
 
 	p = (a + n - 1);
 
-	for (i = n - 1; i > 0; i++)
+	for (i = 0; i <= n - 1; i++)
 	{
 		*(temp + i) = *p;
 		p--;
 	}
-	for (i = 0; i < n - 1; i++)
+	for (i = 0; i <= n - 1; i++)
 	{
-		*(a + i) = *temp;
-		temp[0]++;
+		*(a + i) = temp[i];
 	}
 }
