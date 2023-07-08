166706dedaf79daf69acdb8d04145a81c691a0b9
king <akingbenidavidoyin@gmail.com>
2023-07-08 04:52:10 +0000

    print num of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+*main - prints number of variables
+*@argc: int number of int
+*@argv: list vector of arguments
+*
+*Return: 0, if success
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
