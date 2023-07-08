35eca0ddf75a9e620aa7a0189031a696d1efe702
king <akingbenidavidoyin@gmail.com>
2023-07-08 13:32:11 +0000

    uses minus instead of modulus

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -28,28 +28,23 @@ int main(int argc, char *argv[])
 	{
 		if (cents >= 25)
 		{
-			count = count + (cents / 25) - 1;
-			cents %= 25;
+			cents -= 25;
 		}
 		else if (cents >= 10)
 		{
-			count = count + (cents / 10) - 1;
-			cents %= 10;
+			cents -= 10;
 		}
 		else if (cents >= 5)
 		{
-			count = count + (cents / 5) - 1;
-			cents %= 5;
+			cents -= 5;
 		}
 		else if (cents >= 2)
 		{
-			count = count + (cents / 2) - 1;
-			cents %= 2;
+			cents -= 2;
 		}
 		else if (cents >= 1)
 		{
-			count = count + (cents / 1) - 1;
-			cents %= 1;
+			cents -= 1;
 		}}
 	printf("%d\n", count);
 	return (0);
