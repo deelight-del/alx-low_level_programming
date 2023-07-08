0a2a09e1d0faed7ff7b41a87a7681122829bebcd
king <akingbenidavidoyin@gmail.com>
2023-07-08 04:47:03 +0000

    print name with main

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+*main - prints name of program
+*@argc: number of variable
+*@argv: array of variable
+*
+*Return: 0, if success
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
