88fb1b28cee8376cf234a08ff399038fd7718045
king <akingbenidavidoyin@gmail.com>
2023-07-08 06:22:05 +0000

    addition of argv

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+*main - adds the positive numbers
+*@argc: number of arguments
+*@argv: vector of arguments
+*
+*Return: result from addition
+*/
+int is_str_digit(char *str);
+
+int main(int argc, char *argv[])
+{
+	int result = 0;
+	int i;
+
+	for (i = 1; i < argc; i++)
+		if (is_str_digit(argv[i]))
+		{
+			result += atoi(argv[i]);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	printf("%d\n", result);
+	return (0);
+}
+
+/**
+*is_str_digit - check if string does not conttain char or symbols
+*@str: string to check
+*
+*Return: 1, if success, 0 otherwise
+*/
+
+int is_str_digit(char *str)
+{
+	char *p = str;
+
+	for (; *p != '\0'; p++)
+	{
+		if (!isdigit(*p))
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
