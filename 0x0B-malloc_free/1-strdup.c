881d7a9c9bcc101489aa4fcaecdca88f316a6c17
king <akingbenidavidoyin@gmail.com>
2023-07-10 17:08:54 +0000

    1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+/**
+*_strdup - copies a string to another memory
+*@str: string to copy
+*
+*Return: pointer to string or null
+*/
+
+char *_strdup(char *str)
+{
+	int len = strlen(str);
+	int i;
+	char *str_arr = malloc(len);
+
+	if (str == NULL || str_arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < len; i++)
+		str_arr[i] = str[i];
+
+	return (str_arr);
+}
