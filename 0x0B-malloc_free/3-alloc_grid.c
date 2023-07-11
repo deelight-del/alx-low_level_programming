a1ef8793d2fdb6f7a3a4729aa8729a0c347c15c2
king <akingbenidavidoyin@gmail.com>
2023-07-11 11:31:40 +0000

    malloc for 2D array

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+*alloc_grid - allocates memory for 2D array
+*@width: width of array
+*@height: height of array
+*
+*Return: pointer to memory allocated
+*/
+
+int **alloc_grid(int width, int height)
+{
+	int **mem_array;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	mem_array = malloc(sizeof(int *) * height);
+
+	for (i = 0; i < height; i++)
+		mem_array[i] = malloc(sizeof(int) * width);
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+			mem_array[i][j] = 0;
+	}
+	return (mem_array);
+}
