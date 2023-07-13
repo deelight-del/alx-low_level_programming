848526cb5484abc5118a11ac36ae408c9dc316f0
king <akingbenidavidoyin@gmail.com>
2023-07-13 15:10:16 +0000

    define calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+*_calloc - contigious mem allocation for nmemb
+*@nmemb: number of members of elements
+*@size: size for each member
+*
+*Return: pointer to memory allocated
+*/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *mem_ptr;
+	unsigned int i;
+
+	(void) i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	mem_ptr = malloc(nmemb * size);
+
+	if (mem_ptr == NULL)
+		return (NULL);
+	memset(mem_ptr, 0, nmemb);
+	return (mem_ptr);
+}
