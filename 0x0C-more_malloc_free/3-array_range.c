7a1471e6648a642acfbd62d3ead759626f1532c7
king <akingbenidavidoyin@gmail.com>
2023-07-13 15:47:42 +0000

    allocated 44 instead of 41

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -17,7 +17,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	mem_ptr = malloc((sizeof(int) * size) + 1);
+	mem_ptr = malloc(sizeof(int) * (size + 1));
 
 	if (mem_ptr == NULL)
 		return (NULL);
