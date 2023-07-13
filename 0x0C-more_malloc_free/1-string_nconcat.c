a911148b0e0e1124e1092615f1475ee1a6ede59b
king <akingbenidavidoyin@gmail.com>
2023-07-13 14:02:05 +0000

    catenate str

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+*string_nconcat - concat s1 and n part of s2
+*@s1: first string to cat
+*@s2: second string to cat
+*n: number of s2 to cat
+*
+*/
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *mem;
+	unsigned int i, m;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	if (n >= strlen(s2))
+		n = strlen(s2);
+
+	m = strlen(s1);
+
+	mem = malloc((sizeof(char) * (m + n)) + 1);
+
+	if (mem == NULL)
+		return (NULL);
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		mem[i] = s1[i];
+	}
+
+	for (i = 0; m < (m + n); m++, i++)
+	{
+		mem[m] = s2[i];
+	}
+	mem[m] = '\0';
+
+	return mem;
+}
