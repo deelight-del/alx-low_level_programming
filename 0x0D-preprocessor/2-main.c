a88cac6e6b860bd22847392a4df79f9a0a996f7f
king <akingbenidavidoyin@gmail.com>
2023-07-17 16:32:27 +0000

    file name macro

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main - prints the name of the file of compilation
+*
+*Return: 0, if successful.
+*/
+
+int main(void)
+{
+	printf("%s\n",__FILE__);
+	return (0);
+}
