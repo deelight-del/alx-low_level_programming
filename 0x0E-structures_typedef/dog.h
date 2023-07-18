8440a0ad4c77f5dd754e387f83b1d62e276354f7
king <akingbenidavidoyin@gmail.com>
2023-07-18 14:22:23 +0000

    dog struct in header file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+* struct dog - structure for dog profile
+* @name: dog name
+* @age: dog age
+* @owner: owner of the dog
+*
+*Description: To categorise each dog accoring to respective profile
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
