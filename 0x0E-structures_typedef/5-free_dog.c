cdca3b70649351fca725dca7ecfa34477928014a
king <akingbenidavidoyin@gmail.com>
2023-07-18 16:26:15 +0000

    free struct d

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+*free_dog - frees pointer to struct dog
+*@d: pointer to struct dog variable
+*
+*Return: void
+*/
+
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
