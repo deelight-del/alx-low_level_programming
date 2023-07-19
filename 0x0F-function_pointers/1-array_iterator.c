e6527d08b4989ca3c092dbe630d87dc349a2cfad
king <akingbenidavidoyin@gmail.com>
2023-07-19 14:50:46 +0000

    action function

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+*array_iterator - iterate upon each array and
+*perform some certain action as given by fuct pointer action
+*@array: array pointer given
+*@size: size of the array
+*@action: function pointer of array given
+*
+*Return: Void
+*/
+
+void array_iterator(int *array, size_t size, void(*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(*(array + i));
+	}
+}
