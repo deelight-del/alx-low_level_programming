8157b762a566e600205106bf6be2809ed6355bbf
king <akingbenidavidoyin@gmail.com>
2023-07-19 15:36:43 +0000

    search for index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+
+/**
+*int_index - function that returns index of value in an array
+*that cmp function pointer compares for
+*@array: array to be compared
+*@size: size of the array
+*@cmp: comparism function pointer
+*
+*Return: int, which is the index of value cmp is defined for
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0 || array == NULL)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+	return (-1);
+}
