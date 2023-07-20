506d15d4128500149adcc4a6c42fc3622109136c
king <akingbenidavidoyin@gmail.com>
2023-07-20 13:37:55 +0000

    fixed if str is null

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -24,7 +24,10 @@ void print_strings(const char *seperator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		str = va_arg(args, char *);
-		printf("%s", str);
+		if (str == NULL)
+			printf("(nil)");
+		else
+			printf("%s", str);
 		if (seperator != NULL && i != n - 1)
 		{
 			printf("%s", seperator);
