e59ac18d0a7a780f9a978a2d2ba432bf4a7c61f9
deelight-del <akingbenidavidoyin@gmail.com>
2023-07-28 09:13:21 +0000

    print the number of elements in a node, and change task 0 to return 0 when ptr is Null

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,20 +3,18 @@
 #include "lists.h"
 
 /**
-*print_list - function to print the elements of a node
+*list_len - function to print the length of elmementss of a list.
 *@h: ptr that points to the first node, and invariable
 *all the other nodes through forward search
 *
 *Return: The number of nodes
 */
 
-size_t print_list(const list_t *h)
+size_t list_len(const list_t *h)
 {
 	/*define ptr to list_t*/
 	const list_t *nodePtr;
 	size_t i;
-	unsigned int len;
-	char *string;
 
 	if (h == NULL)
 		return (0);
@@ -25,19 +23,6 @@ size_t print_list(const list_t *h)
 
 	/*access the first node*/
 	for (i = 0; nodePtr; i++)
-	{
-		if (!nodePtr->str)
-		{
-			len = 0;
-			string = "(nil)";
-		}
-		else
-		{
-			len = nodePtr->len;
-			string = nodePtr->str;
-		}
-		printf("[%d] %s\n", len, string);
 		nodePtr = nodePtr->next;
-	}
 	return (i);
 }
