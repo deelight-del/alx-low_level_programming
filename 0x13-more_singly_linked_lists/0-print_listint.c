cbb5dec6fb59663de873beba397b1e051b8b9fff
deelight-del <akingbenidavidoyin@gmail.com>
2023-07-31 21:37:21 +0000

    print int

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stddef.h>
+
+/**
+  * print_list - function to print the eelement of a list_int LL
+  * @h: A head pointer that  points to the first node
+  *
+  *Return: len of elements in a typical LL
+  */
+
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *temp = h;
+	size_t count = 0;
+
+	while (temp != NULL)
+	{
+		printf("%d\n", temp->n);
+		count++;
+		temp = temp->next;
+	}
+
+	return (count);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+Readme for more singly lists
