38364d804cadfa7ad6019af8f86358bafcfe6fe7
deelight-del <akingbenidavidoyin@gmail.com>
2023-08-01 01:11:24 +0000

    free list 2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+  *free_listint2 - function to free and delete a LL
+  *@head: The adress of the head node
+  *
+  *Return: void
+*/
+
+void free_listint2(listint_t **head)
+{
+	listint_t *h = *head;
+	listint_t *p, *temp = h;
+
+	if (h == NULL)
+		return;
+
+	else
+	{
+		while (p)
+		{
+			p = temp->next;
+			free(temp);
+			temp = p;
+		}
+		*head = NULL;
+	}
+}
