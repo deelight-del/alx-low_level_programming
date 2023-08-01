f33ab0557acf69028033bc77f49f3d11c5bd4f66
deelight-del <akingbenidavidoyin@gmail.com>
2023-08-01 13:22:54 +0000

    added function to count list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -45,3 +45,25 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	return (*head);
 }
+
+
+/**
+  * listint_len - function to print the eelement of a list_int LL
+  * @h: A head pointer that  points to the first node
+  *
+  *Return: len of elements in a typical LL
+  */
+
+size_t listint_len(const listint_t *h)
+{
+	const listint_t *temp = h;
+	size_t count = 0;
+
+	while (temp != NULL)
+	{
+		count++;
+		temp = temp->next;
+	}
+
+	return (count);
+}
