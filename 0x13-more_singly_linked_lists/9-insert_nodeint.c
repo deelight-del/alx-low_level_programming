10722ad00775abbcc9f23a9dcea146f6f42674f1
deelight-del <akingbenidavidoyin@gmail.com>
2023-08-02 11:17:46 +0000

    fix null pass

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -3,30 +3,34 @@
 #include "lists.h"
 
 /**
-  *insert_nodeint_at_index - functon to add node at given index
-  *@head: address of the head node pointer
-  *@idx: index of node to add new node
-  *@n: value to add to new node
-  *
-  *Return: A pointer to a listint_t datatype
-*/
+ *insert_nodeint_at_index - functon to add node at given index
+ *@head: address of the head node pointer
+ *@idx: index of node to add new node
+ *@n: value to add to new node
+ *
+ *Return: A pointer to a listint_t datatype
+ */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *newnode, *temp;
 	unsigned int i;
 
-/*	if (head == NULL)
-*		return (NULL);
-*/
-	if (((size_t)idx) >= listint_len(*head))
-		return (NULL);
-
 	newnode = malloc(sizeof(listint_t));
 	if (newnode == NULL)
 		return (NULL);
 	newnode->n = n;
 
+	if (head == NULL || *head == NULL)
+	{
+		*head = newnode;
+		return (newnode);
+	}
+	if (((size_t)idx) >= listint_len(*head))
+	{
+		free(newnode);
+		return (NULL);
+	}
 	if (idx == 0)
 	{
 		newnode->next = *head;
@@ -50,11 +54,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 
 /**
-  * listint_len - function to print the eelement of a list_int LL
-  * @h: A head pointer that  points to the first node
-  *
-  *Return: len of elements in a typical LL
-  */
+ * listint_len - function to print the eelement of a list_int LL
+ * @h: A head pointer that  points to the first node
+ *
+ *Return: len of elements in a typical LL
+ */
 
 size_t listint_len(const listint_t *h)
 {
