e14c70504ea33ed35345c7145066fb50a7abaa3d
deelight-del <akingbenidavidoyin@gmail.com>
2023-08-04 10:59:36 +0000

    fix betty errors

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,13 +8,12 @@
   *Return: void
 */
 
-void print_binary(unsigned long int n) 
+void print_binary(unsigned long int n)
 {
-	    if (n > 1) 
-	    {
-		    print_binary(n >> 1);
-			        
-	    }
-	        _putchar((n & 1) + 48);
+	if (n > 1)
+	{
+		print_binary(n >> 1);
+	}
+	_putchar((n & 1) + 48);
 }
 
