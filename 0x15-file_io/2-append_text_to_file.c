216d509f0f23cf5294299ee4862d1ec02b24f19d
deelight-del <akingbenidavidoyin@gmail.com>
2023-08-07 22:44:29 +0000

    add conditional when text is not null to task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -23,13 +23,15 @@ int append_text_to_file(const char *filename, char *text_content)
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
-
-	n_write = write(fd, text_content, strlen(text_content));
-
-	if (n_write == -1)
+	if (text_content != NULL)
 	{
-		close(fd);
-		return (-1);
+		n_write = write(fd, text_content, strlen(text_content));
+
+		if (n_write == -1)
+		{
+			close(fd);
+			return (-1);
+		}
 	}
 	close(fd);
 	return (1);
