16c3ff8ee56730faf70a7a243c08b63fe0a178e7
deelight-del <akingbenidavidoyin@gmail.com>
2023-08-07 20:55:17 +0000

    first push for writing and reading from a file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+/**
+  *read_textfile - Read file and prints it to stdout
+  *@filename: The name of the file to read from
+  *@letters: The numver of letters it should read and print
+  *
+  *Return: A fd if success, but -1 if it fails
+  */
+
+ssize_t read_textfile(const char *filename, size_t letter)
+{
+	ssize_t n_ret;
+	int fd;
+	char *str;
+
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	str = malloc(sizeof(char) * letter);
+	if (str == NULL)
+	{
+		free (str);
+		close (fd);
+		return (0);
+	}
+	n_ret = read(fd, str, letter);
+
+	dprintf(STDOUT_FILENO, "%s", str);
+	close (fd);
+	return (n_ret);
+}
