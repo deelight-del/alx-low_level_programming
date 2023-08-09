a3f60b4fa26da88b0bf2812907bde1dd8f085f9a
deelight-del <akingbenidavidoyin@gmail.com>
2023-08-09 11:50:30 +0000

    xxx

--- 0x15-file_io/0-main.c
+++ 0x15-file_io/0-main.c
@@ -7,18 +7,12 @@
  *
  * Return: Always 0.
  */
-int main(int ac, char **av)
+int main(void)
 {
 	ssize_t n;
 
-	if (ac != 2)
-	{
-		dprintf(2, "Usage: %s filename\n", av[0]);
-		exit(1);
-	}
-	n = read_textfile(av[1], 114);
+	n = read_textfile(NULL, 10);
 	printf("\n(printed chars: %li)\n", n);
-	n = read_textfile(av[1], 1024);
-	printf("\n(printed chars: %li)\n", n);
+
 	return (0);
 }
