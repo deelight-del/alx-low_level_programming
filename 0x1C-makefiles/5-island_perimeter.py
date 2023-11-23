9404d276deb4978f9c282b03a6a8e3ce40f07748
deelight-del <akingbenidavidoyin@gmail.com>
2023-11-23 04:40:04 +0000

    task 100

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" This is the module for technical
+interview on calculating the island perimeter"""
+def island_perimeter(grid):
+    """ The island perimeter to calculate the
+    land perimeter of the island.
+    Arg:
+        grid - definiiton of the grid of grid.
+
+    Return:
+        The perimter of the island grid.
+    """
+
