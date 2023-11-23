101b987190d1c6a7cc8cc45e9978d7918b681c95
deelight-del <akingbenidavidoyin@gmail.com>
2023-11-23 08:53:43 +0000

    task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -11,3 +11,20 @@ def island_perimeter(grid):
         The perimter of the island grid.
     """
 
+    perimeter = 0
+    i_thresh = len(grid)
+    for i, single_list in enumerate(grid):
+        j_thresh = len(single_list)
+        for j, tile in enumerate(single_list):
+            if tile == 1:
+                p_contribution = 4
+                if (j != j_thresh) and single_list[j + 1] == 1:
+                    p_contribution -= 1
+                if (j != 0) and single_list [j - 1] == 1:
+                    p_contribution -= 1
+                if (i != 0) and  grid[i - 1][j] == 1:
+                    p_contribution -= 1
+                if (i != i_thresh) and grid[i + 1][j] == 1:
+                    p_contribution -= 1
+                perimeter += p_contribution
+    return perimeter
