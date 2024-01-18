5b7ae71df7e605b37554ca67f5de2a8e6a189539
deelight-del <akingbenidavidoyin@gmail.com>
2024-01-18 09:19:25 +0000

    fix search algo

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -31,7 +31,6 @@ typedef struct listint_s
  * @index: Index of the node in the list
  * @next: Pointer to the next node
  * @express: Pointer to the next node in the express lane
- * 
  * Description: singly linked list node structure with an express lane
  */
 typedef struct skiplist_s
